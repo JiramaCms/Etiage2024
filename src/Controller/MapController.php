3b5d52f036785de4408206920d4eb9f05919ca3c
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2024-07-15 15:51:37 +0300

    add point

---
+++ src/Controller/MapController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Point;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MapController extends AbstractController
+{
+    #[Route('/map', name: 'app_map')]
+    public function index(ManagerRegistry $mr): Response
+    {
+        $allPoints = $mr->getRepository(Point::class)->findAll();
+        return $this->render('map/index.html.twig', [
+            'allPoints' => $allPoints,
+        ]);
+    }
+
+    #[Route('/map/savePoint', name: 'point_save')]
+    public function savePoint(Request $request,ManagerRegistry $mr): Response
+    {
+        $latitude = $request->request->get('latitude');
+        $longitude = $request->request->get('longitude');
+        $nom = $request->request->get('nom');
+
+        if ($latitude && $longitude && $nom) {
+            $points = new Point();
+            $points->setLatitude((float) $latitude);
+            $points->setLongitude((float) $longitude);
+            $points->setNom($nom);
+            $em = $mr->getManager();
+
+            $em->persist($points);
+            $em->flush();
+
+            $this->addFlash('success', 'Location saved successfully!');
+        }
+
+        return $this->redirectToRoute('app_map');
+    }
+}
---
+++ src/Entity/Point.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PointRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PointRepository::class)]
+class Point
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?float $latitude = null;
+
+    #[ORM\Column]
+    private ?float $longitude = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getLatitude(): ?float
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(float $latitude): static
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?float
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(float $longitude): static
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+}
