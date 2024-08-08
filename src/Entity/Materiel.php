44efe69c8ddbe7a2d6b9ad3ef8313d6a22255f5d
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2024-08-08 11:07:51 +0300

    ajout des materiels

---
+++ src/Entity/Materiel.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MaterielRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MaterielRepository::class)]
+class Materiel
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $libelle = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $code = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $capacite = null;
+
+    #[ORM\Column]
+    private ?bool $disposition = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): static
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getCapacite(): ?int
+    {
+        return $this->capacite;
+    }
+
+    public function setCapacite(?int $capacite): static
+    {
+        $this->capacite = $capacite;
+
+        return $this;
+    }
+
+    public function isDisposition(): ?bool
+    {
+        return $this->disposition;
+    }
+
+    public function setDisposition(bool $disposition): static
+    {
+        $this->disposition = $disposition;
+
+        return $this;
+    }
+}
